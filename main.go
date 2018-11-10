480c3f3ca1cdf62ee029f43877223ffbbf8ff105
tauraamui <tauracatamui@gmail.com>
2018-11-10 09:18:17 +0000

    apparently connecting direct to guard gateways is error prone, using tor bin instead

--- main.go
+++ main.go
@@ -1,29 +1,68 @@
 package main
 
 import (
-	"bufio"
-	"os"
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
 
-	"github.com/tacusci/logging"
+	"github.com/cretz/bine/tor"
+	"golang.org/x/net/html"
 )
 
-var torEntryNodeIPs []string
-
 func main() {
-	file, err := os.Open("entrynodeslist.txt")
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	// Start tor with default config (can set start conf's DebugWriter to os.Stdout for debug logs)
+	fmt.Println("Starting tor and fetching title of https://check.torproject.org, please wait a few seconds...")
+	t, err := tor.Start(nil, nil)
 	if err != nil {
-		logging.ErrorAndExit(err.Error())
+		return err
 	}
-
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		torEntryNodeIPs = append(torEntryNodeIPs, scanner.Text())
+	defer t.Close()
+	// Wait at most a minute to start network and get
+	dialCtx, dialCancel := context.WithTimeout(context.Background(), time.Minute)
+	defer dialCancel()
+	// Make connection
+	dialer, err := t.Dialer(dialCtx, nil)
+	if err != nil {
+		return err
 	}
-
-	if err := scanner.Err(); err != nil {
-		logging.ErrorAndExit(err.Error())
+	httpClient := &http.Client{Transport: &http.Transport{DialContext: dialer.DialContext}}
+	// Get /
+	resp, err := httpClient.Get("https://check.torproject.org")
+	if err != nil {
+		return err
 	}
+	defer resp.Body.Close()
+	// Grab the <title>
+	parsed, err := html.Parse(resp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Title: %v\n", getTitle(parsed))
+	return nil
+}
+
+func getTitle(n *html.Node) string {
+	if n.Type == html.ElementNode && n.Data == "title" {
+		var title bytes.Buffer
+		if err := html.Render(&title, n.FirstChild); err != nil {
+			panic(err)
+		}
+		return strings.TrimSpace(title.String())
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if title := getTitle(c); title != "" {
+			return title
+		}
+	}
+	return ""
 }
