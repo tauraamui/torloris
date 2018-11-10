e0160609806cde27f38d5a4d170fc1a12023aaba
tauraamui <tauracatamui@gmail.com>
2018-11-10 08:33:14 +0000

    read tor entry node IPs into slice

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"bufio"
+	"os"
+
+	"github.com/tacusci/logging"
+)
+
+var torEntryNodeIPs []string
+
 func main() {
-	
+	file, err := os.Open("entrynodeslist.txt")
+	if err != nil {
+		logging.ErrorAndExit(err.Error())
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		torEntryNodeIPs = append(torEntryNodeIPs, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		logging.ErrorAndExit(err.Error())
+	}
 }
